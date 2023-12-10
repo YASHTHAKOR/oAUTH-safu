32bb1884c73dd86e1b5df7994b674cf2daa80cff
yashthakor <yashthakor4595@gmail.com>
2023-12-10 10:15:10 +0530

    change in private key

--- server/src/commons/utils.ts
+++ server/src/commons/utils.ts
@@ -8,7 +8,7 @@ export const getPublicAddress  = () => {
 
 export const getPublicKey = () => {
     const publicKey = EthCrypto.publicKeyByPrivateKey(
-        '0x107be946709e41b7895eea9f2dacf998a0a9124acbb786f0fd1a826101581a07'
+        PRIVATE_KEY
     );
 
     return publicKey;
